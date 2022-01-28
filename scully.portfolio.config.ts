0194c068b7d78df8167d7c6ed6bb2e0bc62226e3
Hieu Nguyen Vu <nhvu.95@gmail.com>
2022-01-28 21:40:45 +0700

    Add bash code

--- scully.portfolio.config.ts
+++ scully.portfolio.config.ts
@@ -5,6 +5,7 @@ import 'prismjs/plugins/toolbar/prism-toolbar';
 import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
 import 'prismjs/components/prism-yaml';
 import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-bash';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-java';
 import 'prismjs/components/prism-xml-doc';
