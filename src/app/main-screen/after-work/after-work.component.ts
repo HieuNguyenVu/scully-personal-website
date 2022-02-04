17370f0bc1c438bdf654c4fea8e237c5b46957e7
Hieu Nguyen Vu <nhvu.95@gmail.com>
2022-02-04 21:39:01 +0700

    [fix] SEO update

--- src/app/main-screen/after-work/after-work.component.ts
+++ src/app/main-screen/after-work/after-work.component.ts
@@ -1,69 +1,72 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
-import { Observable, of } from 'rxjs';
-import { map, share } from 'rxjs/operators';
-import { MainScreenService } from '../main-screen.service';
-import { Project } from '../portfolio/project.model';
-
+import { Component, Input, OnInit } from "@angular/core";
+import { ScullyRoute, ScullyRoutesService } from "@scullyio/ng-lib";
+import { Observable, of } from "rxjs";
+import { map, share } from "rxjs/operators";
+import { MainScreenService } from "../main-screen.service";
+import { Project } from "../portfolio/project.model";
 
 @Component({
-  selector: 'app-after-work',
-  templateUrl: './after-work.component.html',
-  styleUrls: ['./after-work.component.scss']
+    selector: "app-after-work",
+    templateUrl: "./after-work.component.html",
+    styleUrls: ["./after-work.component.scss"],
 })
 export class AfterWorkComponent implements OnInit {
-  @Input()
-  currentNavIndex = 0;
+    @Input()
+    currentNavIndex = 0;
 
-  constructor(private service: MainScreenService, private scullyService: ScullyRoutesService) { }
-  projects$: Observable<Project[]> = of([]);
-  projectsMore$: Observable<Project[][]> = of([]);
-  ngOnInit(): void {
-    let links$ = this.scullyService.allRoutes$.pipe(
-      map(scullyRoutes => scullyRoutes.filter(scullyRoute => scullyRoute.route.startsWith("/blog"))),
-      map(scullyRoutes => scullyRoutes.sort((postA, postB) => postA.priority - postB.priority)),
-      map(scullyRoutes => scullyRoutes.map(routes => {
-        routes.title = routes.title.replace("　", " ");
-        return routes;
-      })),
-      share());
-    this.projects$ = links$.pipe(
-      map(links => links.slice(0, 5)),
-      map(links => this.scullyRouteTopProject(links)));
-    this.projectsMore$ = links$.pipe(
-      map(links => links.slice(5)),
-      map(links => this.scullyRouteTopProject(links)),
-      map(projects => {
-        let projs = projects;
-        let tripleProj = [];
-        while (true) {
-          let takeData = projs.splice(0, 3);
-          if (takeData.length == 0) {
-            break;
-          }
-          for (let i = 3; i > takeData.length; i--) {
-            takeData.push({ exist: false });
-          }
-          tripleProj.push(takeData);
-        }
-        return tripleProj;
-      })
-    );
-  }
+    constructor(private service: MainScreenService, private scullyService: ScullyRoutesService) {}
+    projects$: Observable<Project[]> = of([]);
+    projectsMore$: Observable<Project[][]> = of([]);
+    ngOnInit(): void {
+        let links$ = this.scullyService.allRoutes$.pipe(
+            map((scullyRoutes) => scullyRoutes.filter((scullyRoute) => scullyRoute.route.startsWith("/blog"))),
+            map((scullyRoutes) => scullyRoutes.sort((postA, postB) => postA.priority - postB.priority)),
+            map((scullyRoutes) =>
+                scullyRoutes.map((routes) => {
+                    routes.title = routes.title.replace("　", " ");
+                    return routes;
+                })
+            ),
+            share()
+        );
+        this.projects$ = links$.pipe(
+            map((links) => links.slice(0, 5)),
+            map((links) => this.scullyRouteTopProject(links))
+        );
+        this.projectsMore$ = links$.pipe(
+            map((links) => links.slice(5)),
+            map((links) => this.scullyRouteTopProject(links)),
+            map((projects) => {
+                let projs = projects;
+                let tripleProj = [];
+                while (true) {
+                    let takeData = projs.splice(0, 3);
+                    if (takeData.length == 0) {
+                        break;
+                    }
+                    for (let i = 3; i > takeData.length; i--) {
+                        takeData.push({ exist: false });
+                    }
+                    tripleProj.push(takeData);
+                }
+                return tripleProj;
+            })
+        );
+    }
 
-  scullyRouteTopProject(scullyRoutes: ScullyRoute[]): Project[] {
-    return scullyRoutes.map<Project>(scullyRoute => {
-      return {
-        title: scullyRoute.title.replace("<br>", " "),
-        image: scullyRoute.image,
-        tags: scullyRoute.tags,
-        description: scullyRoute.description,
-        date_start: scullyRoute.date_start,
-        date_end: scullyRoute.date_end,
-        link: scullyRoute.route,
-        exist: scullyRoute.published,
-        priority: scullyRoute.priority
-      };
-    });
-  }
+    scullyRouteTopProject(scullyRoutes: ScullyRoute[]): Project[] {
+        return scullyRoutes.map<Project>((scullyRoute) => {
+            return {
+                title: scullyRoute.title.replace("<br>", " "),
+                image: scullyRoute.image,
+                tags: scullyRoute.tags,
+                description: scullyRoute.description,
+                date_start: scullyRoute.date_start,
+                date_end: scullyRoute.date_end,
+                link: scullyRoute.route,
+                exist: scullyRoute.published,
+                priority: scullyRoute.priority,
+            };
+        });
+    }
 }
--- src/app/main-screen/after-work/afterwork-item.component.ts
+++ src/app/main-screen/after-work/afterwork-item.component.ts
@@ -1,15 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Project } from '../portfolio/project.model';
+import { Component, ElementRef, Input, OnInit, SimpleChanges, ViewChild } from "@angular/core";
+import { Observable } from "rxjs";
+import { Project } from "../portfolio/project.model";
 
 @Component({
-  selector: 'app-afterwork-item',
-  templateUrl: 'afterwork-item.component.html',
-  styleUrls: ['./afterwork-item.component.scss']
+    selector: "app-afterwork-item",
+    templateUrl: "afterwork-item.component.html",
+    styleUrls: ["./afterwork-item.component.scss"],
 })
 export class AfterWorkItemComponent implements OnInit {
+    @ViewChild("topicImage") topicImage: ElementRef<HTMLElement>;
+    project: Project;
 
-  constructor() { }
-  @Input() project: Project;
-  ngOnInit(): void {
-  }
+    constructor() {}
+    ngOnInit(): void {}
+
+    ngOnChanges(changes: SimpleChanges) {
+        console.log(changes.project);
+        this.topicImage.nativeElement.style.backgroundImage = `url('${(<Project>changes.project).image}')`;
+    }
 }
