9af9a9d4199ce463dc3d124c7fc7037b5def6890
Vue Nguyen <nhvu.95@gmail.com>
2022-02-04 12:48:28 +0000

    [skip ci] deploy gh-pages

--- main-screen-main-screen-module.js
+++ main-screen-main-screen-module.js
@@ -1476,6 +1476,7 @@ let AfterWorkComponent = class AfterWorkComponent {
         this.projectsMore$ = links$.pipe(Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_6__["map"])(links => links.slice(6)), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_6__["map"])(links => this.scullyRouteTopProject(links)), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_6__["map"])(projects => {
             let projs = projects;
             let tripleProj = [];
+            console.log(projects);
             while (true) {
                 let takeData = projs.splice(0, 3);
                 if (takeData.length == 0) {
